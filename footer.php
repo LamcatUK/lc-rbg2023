c7231ce9eebc968eef07381b21631b04852e6cd2
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-08-03 11:33:09 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -61,13 +61,15 @@ defined('ABSPATH') || exit;
     </svg></a>
 <?php wp_footer();
 if (get_field('gtm_property', 'options')) {
-    ?>
+    if (!is_user_logged_in()) {
+        ?>
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe
         src="https://www.googletagmanager.com/ns.html?id=<?=get_field('gtm_property', 'options')?>"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<?php
+        <?php
+    }
 }
 ?>
 </body>
