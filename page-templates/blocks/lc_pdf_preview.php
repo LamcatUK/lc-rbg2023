d13251681b932dde5d9f734cf43e28809fab2698
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-08-04 11:51:22 +0100

    pdf preview

--- page-templates/blocks/lc_pdf_preview.php
+++ page-templates/blocks/lc_pdf_preview.php
@@ -6,9 +6,7 @@ $link = get_field('link');
 <section class="pdf_preview py-5">
     <div class="container-xl">
         <div class="pdf_preview__container">
-            <div class="pdf_preview__title">
-                <?=get_field('title')?>
-            </div>
+            <a href="<?=$pdf_url?>" target="_blank" class="pdf_preview__title"><?=get_field('title')?></a>
             <a class="btn btn-gold"
                 href="<?=$link['url']?>"
                 target="<?=$link['target']?>"><?=$link['title']?> <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
